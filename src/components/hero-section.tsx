a961e99767cc72c34cd0159858265f376f899dae
Hamiz Ali <hamiz.ali686@gmail.com>
2024-08-13 15:57:15 +0200

    first draft

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import NavBar from './nav-bar';
 import AvatarSection from './avatar-section';
+import { Card } from './ui/card';
 
 const HeroSection = () => {
   return (
-    <div className='h-max w-full flex-1 flex-col rounded-b-[7rem] border-b border-opacity-10 bg-secondary pb-20'>
+    <Card className='h-max w-full flex-1 flex-col rounded-b-[7rem] border-b border-opacity-10 pb-20'>
       <div className='mx-auto flex max-w-[90%] flex-col space-y-28'>
         <NavBar />
         <div>
           <AvatarSection />
         </div>
       </div>
-    </div>
+    </Card>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import HeroSection from '@/components/hero-section';
+import { Carousel } from '@/components/ui/carousel';
+import CarouselSection from '@/components/carousel-section';
+import ServicesSection from '@/components/services-section';
+import FooterSection from '@/components/footer-section';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <div className='flex w-full flex-1 flex-col'>
-      <HeroSection />
-      <div className='min-h-40'></div>
+      <div className='bg-secondary'>
+        <HeroSection />
+      </div>
+      <div className='flex w-full rounded-b-[6rem] border-b bg-secondary px-2 py-12'>
+        <CarouselSection />
+      </div>
+      <div className='px-2 py-24'>
+        <ServicesSection />
+      </div>
+      <div className='flex h-full w-full flex-1 px-2'>
+        <FooterSection />
+      </div>
     </div>
   );
 }
