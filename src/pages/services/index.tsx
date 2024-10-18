c3bb21d8141f94d924bcb33de82581e8b6b20d3f
blink616 <hamiz@fruits.co>
2024-10-18 22:24:30 +0200

    fix sitemap

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/navigation';
 
 const Service = () => {
@@ -9,3 +10,21 @@ const Service = () => {
 };
 
 export default Service;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return new Promise((resolve) => {
+    try {
+      resolve({
+        redirect: {
+          destination: '/services/web-development',
+          permanent: true,
+        },
+      });
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (e) {
+      resolve({
+        notFound: true,
+      });
+    }
+  });
+};
